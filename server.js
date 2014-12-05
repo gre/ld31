2ef46fea801c3a7fb287412974f64838723e88f3
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-12-05 19:53:26 +0100

    server

---
+++ server.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+
+var MongoClient = require('mongodb').MongoClient;
+var express = require("express");
+var fs = require("fs");
+var Q = require("q");
+
+var app = express();
+app.use(require("body-parser").json());
+
+var MONGO = 'mongodb://127.0.0.1:27017/ld31';
+var COLL = "scores";
+
+var connectMongo = Q.nbind(MongoClient.connect, MongoClient);
+
+connectMongo(MONGO)
+  .then(function (db) {
+    return Q.ninvoke(db.collection(COLL).find(), "toArray");
+  })
+  .then(function (res) {
+    console.log("Nb Entries: "+res.length);
+  })
+  .done();
+
+
+app.get("/", function (req, res) {
+  res.send("Nothing here.");
+});
+
+app.get("/scores", function (req, res) {
+  connectMongo(MONGO)
+  .then(function (db) {
+    var collection = db.collection(COLL);
+    return Q.ninvoke(collection.find(), "toArray");
+  })
+  .then(function (results) {
+    res.send(JSON.stringify(results));
+  })
+  .fail(function (e) {
+    console.log(e)
+    console.log(e.stack);
+    res.status(400).send(e.toString());
+  })
+  .done();
+});
+
+app.put("/scores", function (req, res) {
+  connectMongo(MONGO)
+  .then(function (db) {
+    var item = req.body;
+    if (
+      typeof item.player === "string" && /^[a-z0-9]{3,20}$/.exec(item.player) &&
+      typeof item.x === "number" &&
+      typeof item.y === "number" &&
+      0 <= item.x && item.x <= 320 &&
+      200 < item.y
+    ) {
+      var collection = db.collection(COLL);
+      item = {
+        player: item.player,
+        x: item.x,
+        y: item.y
+      };
+      return Q.ninvoke(collection, "insert", item).thenResolve(item);
+    }
+    else {
+      throw new Error("score requirement: { player /* alphanum in 3-20 chars */, x, y }");
+    }
+  })
+  .then(function (item) {
+    console.log(item);
+    res.send();
+  })
+  .fail(function (e) {
+    console.log(e)
+    console.log(e.stack);
+    res.status(400).send(e.toString());
+  })
+  .done();
+});
+
+app.listen(9832);
